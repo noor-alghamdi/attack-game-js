310b9e8a16748060c499ce6083b9c5a956b53826
noor-aghamdi <namacoconut@gmail.com>
2023-05-22 14:36:45 +0300

    attack handler func

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,3 +3,12 @@ let chosenMaxLife = 100
 let currentMonsterHealth = chosenMaxLife
 let currentPlayerHealth = chosenMaxLife
 
+// calling function and assign variable name as a parameter value instead of hard coded values
+adjustHealthBars(chosenMaxLife)
+
+function attackHandler(){
+    const damage = dealMonsterDamage(ATTACK_VALUE)
+    currentMonsterHealth -= damage
+}
+
+attackBtn.addEventListener('click',attackHandler)

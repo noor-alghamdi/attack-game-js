e8acf779a2e40c96af845a595ba92e2896b13c96
noor-aghamdi <namacoconut@gmail.com>
2023-05-31 10:14:44 +0300

    modify shorten attack funstion

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,4 @@
-const PLAYER_ATTACK_VALUE = 10
-const MONSTER_ATTACK_VALUE = 10
+const ATTACK_VALUE = 10
 const STRONG_ATTACK = 100
 let chosenMaxLife = 100
 let currentMonsterHealth = chosenMaxLife
@@ -8,37 +7,37 @@ let currentPlayerHealth = chosenMaxLife
 // calling function and assign variable name as a parameter value instead of hard coded values
 adjustHealthBars(chosenMaxLife)
 
+// Attack handler for both moster and player to removee the redundant code
+function attackMonster(mode){
+let maxDamage
+if (mode === 'ATTACK'){
+    maxDamage = ATTACK_VALUE
+} else if (mode === 'STRONG_ATTACK'){
+    maxDamage = STRONG_ATTACK
+}
+const playerDamage = dealMonsterDamage(maxDamage)
+currentMonsterHealth -= playerDamage
+
+const monsterDamage = dealPlayerDamage(maxDamage)
+currentPlayerHealth -= monsterDamage
+
+if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
+    alert('you win!')
+}else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
+    alert ('you lose!')
+     
+} else if ( currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
+    alert('draw!')
+}
+
+}
+
 function attackHandler(){
-    const playerDamage = dealMonsterDamage(PLAYER_ATTACK_VALUE)
-    currentMonsterHealth -= playerDamage
-
-    const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
-    currentPlayerHealth -= monsterDamage
-
-    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
-        alert('you win!')
-    }else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
-        alert ('you lose!')
-         
-    } else if ( currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
-        alert('draw!')
-    }
+    attackMonster('ATTACK')
 }
 
 function strongAttackHandler(){
-    const playerDamage = dealMonsterDamage(STRONG_ATTACK)
-    currentMonsterHealth -= playerDamage
-
-    const monsterDamage = dealPlayerDamage(STRONG_ATTACK)
-    currentPlayerHealth -= monsterDamage
-
-    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
-        alert('you win!')
-    }else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
-        alert ('you lose!')
-    } else if ( currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
-        alert('draw!')
-    }
+   attackMonster('STRONG_ATTACK')
 }
 
 

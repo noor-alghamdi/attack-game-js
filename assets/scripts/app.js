0932c9c24753c7b29f511176cd9e437bc8f6632f
noor-aghamdi <namacoconut@gmail.com>
2023-06-13 10:27:56 +0300

    finished

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -234,24 +234,7 @@ function printLogHandler() {
     console.log('------------');
   }
   let j = 0;
-  // outerWhile: do {
-  //   console.log('Outer', j);
-  //   innerFor: for (let k = 0; k < 5; k++) {
-  //     if (k === 3) {
-  //       // break outerWhile;
-  //       // continue outerWhile; // dangerous! => Infinite loop!
-  //     }
-  //     console.log('Inner', k);
-  //   }
-  //   j++;
-  // } while (j < 3);
-  // for (let i = 10; i > 0;) {
-  //   i--;
-  //   console.log(i);
-  // }
-  // for (let i = 0; i < battleLog.length; i++) {
-  //   console.log(battleLog[i]);
-  // }
+
   let i = 0;
   for (const logEntry of battleLog) {
     if ((!lastLoggedEntry && lastLoggedEntry !== 0) || lastLoggedEntry < i) {
